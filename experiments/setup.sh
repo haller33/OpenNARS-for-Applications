118a7384e0d1e9a5fc370283a9db84be5c80700f
CHANGEME <CHANGEME>
2026-05-11 21:58:47 -0300

    feat: add experiments folder with some narseses tests also

---
+++ experiments/setup.sh
@@ -0,0 +1,2 @@
+python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('wordnet'); nltk.download('omw-1.4')"
+python -c "import nltk; nltk.download('averaged_perceptron_tagger_eng')"
